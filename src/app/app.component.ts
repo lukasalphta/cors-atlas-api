3235dda1339b724fa5bbbc0c8e50b81cad0ce9e1
Lukasz <l.gawrys1@gmail.com>
2019-04-29 12:02:07 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cors-atlas';
+  apiKey = 'xxx';
+
+  constructor(private httpClient: HttpClient) {
+    const headers = new HttpHeaders().set('x-origin', '1');
+
+    const httpOptions = {
+      headers
+    };
+    this.httpClient
+    .get('https://atlas.microsoft.com/search/address/structured/json?subscription-key='+ this.apiKey +'&api-version=1.0&countryCode=it&postalCode=00012', httpOptions)
+    .subscribe();
+  }
 }
